5cd97fd18fcc3e2556ab82b4c93aa885abdc622b
murarii07 <abhayaj07@gmail.com>
2024-08-25 02:42:52 +0530

    new proj

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import Form from './Form';
+import Nav from './Navbar';
+import Preview from './preview';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+     <Form />
+     <Preview />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Navbar.jsx
@@ -0,0 +1,12 @@
+function Nav(){
+return(
+    <div className="navbar">
+        <ul> 
+            <li>logo</li>
+            <li><a href="/login" target="_blank">login</a></li>
+            <li><a href="/login" target="_blank">signup</a></li>
+        </ul>
+    </div>
+)
+}
+export default Nav;
